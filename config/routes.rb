c116526f05ca3edc44a1473939a5cf1eef8b64e2
Katsuki Fujimoto <kf1996cnbl@gmail.com>
2019-06-23 13:13:49 +0900

    Implement payment api with stripe

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/logout' => 'users#logout'
       post '/facebook' => 'users#facebook'
+      post '/payment' => 'users#add_card'
 
       resources :rooms
     end
